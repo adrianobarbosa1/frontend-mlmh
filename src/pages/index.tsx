d843371e01731d9767bff30286899c7d0a7c32f3
adriano.barbosa100 <adriano.barbosa100@gmail.com>
2022-05-27 15:32:48 -0300

    create page signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,31 @@
-import Head from 'next/head'
+import { Button, Flex, FormControl, FormLabel, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
-    <h1>helo word</h1>
+    <Flex
+      w='100vw'
+      h='100vh'
+      align='center'
+      justify='center'
+    >
+      <Flex
+        as='form'
+        w='100%'
+        maxWidth={360}
+        bg='gray.100'
+        p='8' borderRadius={8}
+        flexDir='column'
+      >
 
+        <Stack spacing='4'>
+          <Input name='email' type='email' label='E-mail' />
+          <Input name='password' type='password' label='Senha' />
+        </Stack>
+
+        <Button type='submit' mt='6' colorScheme='blue'>Entrar</Button>
+
+      </Flex>
+    </Flex>
   )
 }
