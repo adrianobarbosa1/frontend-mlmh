ac90248eb6fcc06e428f1745559322e4d8a24ebb
adriano.barbosa100 <adriano.barbosa100@gmail.com>
2022-06-01 18:44:11 -0300

    att

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,43 @@
+import { Box, Button, Flex, Text, Image } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Footer />
+      <Flex
+        w='100%'
+        h='30rem'
+        background='url(/img/lote.jpg) center/cover no-repeat'
+        direction='column'
+        align='center'
+      >
+
+        <Text pt='2rem' fontSize='4xl' color='text' fontWeight='bold'>
+          Meu Lote Minha História
+        </Text>
+        <Text
+          fontSize='md'
+          color='text'
+          fontWeight='normal'
+          mt='10px'
+          mb='26px'
+          w={{ base: "90%", sm: "60%", lg: "40%", xl: "333px" }}>
+          Use these awesome forms to login or create new account in your project
+          for free.
+        </Text>
+        <Button
+          mb="16px"
+          bg='yellowOficial'
+          color='text'
+          fontSize="xs"
+          variant="no-effects"
+          px="30px"
+        >
+          Cadastrar
+        </Button>
+      </Flex>
+
     </>
   )
 }
